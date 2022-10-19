d043c6a033b6d23830b0478428b1ee57170a575a
Kaaden RumanCam <kaadenruman@hotmail.com>
2022-10-19 14:41:30 -0600

    stripped down version to type up

---
+++ templatesimple.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+#define print(c) for (const auto& e : c) cout << e << ' '
+#define umap unordered_map
+#define uset unordered_set
+#define pb(x) push_back(x)
+#define eb(...) emplace_back(__VA_ARGS__)
+#define mp(x, y) make_pair(x, y)
+#define Rep(b) for(int i = 0; i < (b); ++i)
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
+    
+}
+
