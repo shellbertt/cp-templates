d522b5530bf72107f71108941ce83188dc68f982
Kaaden RumanCam <kaadenruman@hotmail.com>
2022-11-12 10:45:04 -0700

    more stripped

--- templatesimple.cpp
+++ templatesimple.cpp
@@ -9,12 +9,9 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 
 #define print(c) for (const auto& e : c) cout << e << ' '
-#define umap unordered_map
-#define uset unordered_set
 #define pb(x) push_back(x)
 #define eb(...) emplace_back(__VA_ARGS__)
 #define mp(x, y) make_pair(x, y)
-#define Rep(b) for(int i = 0; i < (b); ++i)
 
 int main() {
     cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit);
