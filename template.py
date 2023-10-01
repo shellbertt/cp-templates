5fae25404bcc564a0186670a5aac35e987d31b0a
Kaaden RumanCam <kaadenruman@hotmail.com>
2023-09-30 18:12:59 -0600

    remove code jam print :( etc

--- template.py
+++ template.py
@@ -1,8 +1,9 @@
-import sys, bisect, collections, itertools, math
+import sys, bisect, collections, functools, itertools, math
 input = lambda: sys.stdin.readline().rstrip()
 ints = lambda: list(map(int, input().split()))
 Int = lambda: int(input())
 yn = lambda b: print('YES' if b else 'NO')
-for T in range(1, Int() + 1):
+n = Int()
+for T in range(1, n + 1):
     
-    print(f'Case #{T}:', input())
+
