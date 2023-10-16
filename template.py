d1c70765d315957bb7f72eb8b7c14ef8821a8bf7
Kaaden RumanCam <kaadenruman@hotmail.com>
2023-10-15 21:00:44 -0600

    remove rarely used start of input taking, setrecursionlimit, change to double quotes in yn

--- template.py
+++ template.py
@@ -1,9 +1,8 @@
 import sys, bisect, collections, functools, itertools, math
+sys.setrecursionlimit(1000000)
 input = lambda: sys.stdin.readline().rstrip()
 ints = lambda: list(map(int, input().split()))
 Int = lambda: int(input())
-yn = lambda b: print('YES' if b else 'NO')
-n = Int()
-for T in range(1, n + 1):
-    
+yn = lambda b: print("YES" if b else "NO")
+
 
